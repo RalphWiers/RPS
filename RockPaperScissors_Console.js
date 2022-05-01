c35e033a9b0aa8efaaef48718950bde1ead56371
RalphWiers <ralph.wiersema@hotmail.com>
2022-05-01 11:45:16 +0200

    .

--- RockPaperScissors_Console.js
+++ RockPaperScissors_Console.js
@@ -15,14 +15,18 @@ input to output:
 
 //@ts-check
 
-
+let rockBtn = document.querySelector('.buttonR');
+let paperBtn = document.querySelector('.buttonP');
+let scissorsBtn = document.querySelector('.button');
 let PlayerScore = 0;
 let ComputerScore = 0;
 
-function PlayerChoice() {
+/*function PlayerChoice() {
     let playerSelection = window.prompt('choose Rock, Paper or Scissors').toUpperCase();
-    return playerSelection
-}
+    return playerSelection;
+}*/
+
+let PlayerChoice = '';
 
 function ComputerChoice() {
     let randomNr = Math.floor((Math.random() * 3) + 1); //math.random gives nr. between 0 & 1 / Math.floor makes a full integer
@@ -37,28 +41,42 @@ function ComputerChoice() {
 }
 
 function GameRound(PlayerChoice, ComputerChoice) {
+    console.log(ComputerChoice());
+    console.log(PlayerChoice);
     if (
-        (PlayerChoice === 'ROCK' && ComputerChoice === 'SCISSORS') ||
-        (PlayerChoice === 'PAPER' && ComputerChoice === 'ROCK') ||
-        (PlayerChoice === 'SCISSORS' && ComputerChoice === 'PAPER')
-        ) {
+        (PlayerChoice == 'ROCK' && ComputerChoice == 'SCISSORS') ||
+        (PlayerChoice == 'PAPER' && ComputerChoice ==='ROCK') ||
+        (PlayerChoice == 'SCISSORS' && ComputerChoice == 'PAPER')) {
             PlayerScore++;
-            return 'Player Wins!'
-    } else if (
-        (PlayerChoice === 'ROCK' && ComputerChoice === 'ROCK') ||
-        (PlayerChoice === 'PAPER' && ComputerChoice === 'PAPER') ||
-        (PlayerChoice === 'SCISSORS' && ComputerChoice === 'SCISSORS')
-    ) {
-    return 'Its a tie.'
+            return 'Player Wins!';
+    } else if (PlayerChoice == ComputerChoice) {
+            return 'Its a tie.';
     } else {
-        ComputerScore++;
-        return 'Computer Wins..'
-    } 
-    }
+            ComputerScore++;
+            return 'Computer Wins..';
+    }};
+    
 
-   // Hier cal je de functies pas. De variablen zijn enkel gedefineerd om ze te loggen in de console.
+        document.querySelector('.buttonR').onclick = function() {
+        PlayerChoice = "ROCK";
+        console.log(`PlayerChoice is ${PlayerChoice}`);
+        let game = GameRound(PlayerChoice, ComputerChoice);
+        console.log(game);
+
+        }
+        document.querySelector('.buttonP').onclick = function() {
+        PlayerChoice = "PAPER";
+        console.log(`PlayerChoice is ${PlayerChoice}`);
+        }
+        document.querySelector('.buttonS').onclick = function() {
+        PlayerChoice = "SCISSORS";
+        console.log(`PlayerChoice is ${PlayerChoice}`);
+        }
+
+
+/*
 for (let i = 0; i < 5; i++) {
-var Player = PlayerChoice();
+var Player = PlayerChoice;
 var Computer = ComputerChoice();
 var Round = GameRound(Player, Computer);
 console.log(Player);
@@ -66,4 +84,4 @@ console.log(Computer);
 console.log(Round);
 console.log('PlayerScore ' +  PlayerScore);
 console.log('ComputerScore ' + ComputerScore);
-}
+} */
--- script.js
+++ script.js
@@ -1,28 +1,72 @@
 // @ts-check
 
-//declare variables (playerScore+computerScore)
-//function playround
+//FrontScreen met start (extrapage?)
+//EndScreen met win/lose+restart (conditionloop?/resetScore)
+//Animatie op afbeeldingen
+//fades etc.
 
 let playerScore = 0;
 let computerScore = 0;
 let maxRounds = 5;
+let playerPick;
+
+    const buttons = document.querySelectorAll('div.buttons button');
+    buttons.forEach(button => button.addEventListener('click', playRound));
+    
+    document.querySelector('.buttonR').onclick = function() {
+    playerPick = "ROCK"; 
+    playerImage.src = './images/Rock1.png';}
+    document.querySelector('.buttonP').onclick = function() {
+    playerPick = "PAPER";
+    playerImage.src = './images/Paper1.png' }
+    document.querySelector('.buttonS').onclick = function() {
+    playerPick = "SCISSORS";
+    playerImage.src = './images/Scissors1.png' }
+
+    let winlosetext = document.createElement('p');
+    winlosetext.textContent = 'Who will win?';
+    document.querySelector('.winloseText').appendChild(winlosetext);
+    
+    let PlayerScore = document.createElement('p');
+    PlayerScore.textContent = `PlayerScore: ${playerScore}`;
+    document.querySelector('.scorePlayer').appendChild(PlayerScore);
+    
+    let ComputerScore = document.createElement('p');
+    ComputerScore.textContent = `ComputerScore: ${computerScore}`;
+    document.querySelector('.scoreComputer').appendChild(ComputerScore);
+
+    let playerImage = document.createElement('img');
+    playerImage.src = './images/Hero.jpg';
+    document.querySelector('.playerImage').appendChild(playerImage);
+
+    let computerImage = document.createElement('img');
+    computerImage.src = './images/Computer.jpg';
+    document.querySelector('.computerImage').appendChild(computerImage);
+
+document.getElementById('welcome2').onclick = function() {
+    document.getElementById('StartgameWindow').style.display = 'none';
+}
+
+document.getElementById('restart').onclick = function() {
+    location.reload();
+}
 
 function playRound() {
-    let Pick = prompt("Please pick", "Rock, Paper, Scissors");
-    let playerPick = Pick.toUpperCase();
-    console.log(playerPick);
     let computerChoose = Math.floor(Math.random()*3);
     console.log(`player chooses ${playerPick}`);
     let computerPick;
     switch(computerChoose) {
         case 0: 
             computerPick = 'ROCK';
+            computerImage.src = './images/Rock1.png';
          break;
         case 1:
             computerPick = 'PAPER';
+            computerImage.src = './images/Paper1.png';
          break;
         case 2:
             computerPick = 'SCISSORS';
+            computerImage.src = './images/Scissors1.png';
          break;
     }
     console.log(`computer chooses ${computerPick}`);
@@ -33,6 +77,8 @@ function playRound() {
         (playerPick === 'SCISSORS' && computerPick === 'PAPER')) {
             console.log('player wins round');
             playerScore++;
+            PlayerScore.textContent = `PlayerScore: ${playerScore}`;
+            winlosetext.textContent = 'Player Wins!';
         } 
     else if 
         ((playerPick === 'PAPER' && computerPick === 'SCISSORS') ||
@@ -40,24 +86,24 @@ function playRound() {
         (playerPick === 'ROCK' && computerPick === 'PAPER')) {
             console.log('computer wins round');
             computerScore++;
+            ComputerScore.textContent = `ComputerScore: ${computerScore}`;
+            winlosetext.textContent = 'Computer Wins!';
         }
     else {
         console.log('Its a tie')
+        winlosetext.textContent = 'Its a tie!';
     };
     console.log(`playerScore is ${playerScore}`);
     console.log(`computerScore is ${computerScore}`);
+
+    if(playerScore == 2 || computerScore == 2){ 
+        document.getElementById('EndgameWindow').style.display = 'block';
+        document.getElementById('playerScore').textContent = `PlayerScore: ${playerScore}`;
+        document.getElementById('computerScore').textContent = `ComputerScore: ${computerScore}`;
+    };
 }
-
-function game() {
-for(let i = 0; i < maxRounds; i++) {
-playRound();
-}};
-
-game();
 
 if (playerScore > computerScore) { 
     console.log('Player wins game')} else if
     (playerScore < computerScore) {
     console.log('Computer wins game')};
-
-
